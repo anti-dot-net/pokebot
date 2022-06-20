f0dd1a312c4cb7c6b3cb80a0b189791f970ac0c2
anti-dot-net <107893809+anti-dot-net@users.noreply.github.com>
2022-06-20 19:57:18 -0400

    V2

--- bot.py
+++ bot.py
@@ -1,17 +1,40 @@
 from mastodon import Mastodon
 import pokebase as pb
 import random as rand
-import scehdule
+import schedule
 import time
 
+#Start up Mastodon
+
 mastodon = Mastodon(
-	access_token = 'token.secret'
+	access_token = 'token.secret',
 	api_base_url = 'https://tavern.antinet.work'
 )
 
-def pkmn_otd():
+#Define Pokemon of the Day Function
+
+def pkmn_otd(self):
+	
+	#Pick a random pokemon id
 	
 	pokemon_id = rand.randint(1,151)
+	
+	#Get that pokemon's name
+	
 	pokemon_name = pb.pokemon(pokemon_id)
+	
+	#Pull that pokemons sprite and get the image data for mastodon
+	
 	pokemon_sr = pb.SpriteResource('pokemon', pokemon_id)
-	pokepon_img = pokemon_sr.img_data
+	pokemon_img = pokemon_sr.img_data
+	
+	#create a media post to obtain media_id
+	
+	img_dict = Mastodon.media_post(media_file = pokemon_img, mime_type = '.jpeg')
+	
+	pokemon_text = (('~~~ ')+(pokemon_name)+(' is the Pokemon of the Day!~~~'))
+	
+	mastodon.post_status(pokemon_text)
+	
+	
+pkmn_otd()
